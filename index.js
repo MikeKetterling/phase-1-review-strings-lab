f9f08fa57df214a1d7713479b4d18c9a8bec1890
Mike <mikekett4@yahoo.com>
2021-10-20 10:57:31 -0600

    commit tree e91969f04b4104208bed4798a130338375c9784b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+//import cons from 'cons';
 // Write your code in this file!
+const currentUser = 'Mike Kett';
+const upperCaseCurrentUser = currentUser.toUpperCase();
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`;
+
+const shortGreeting = `Welcome, ${firstInitial}!`;
+
+console.log(shortGreeting);
